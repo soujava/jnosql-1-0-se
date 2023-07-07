759d6681836f4b882151d153f7a7300f6eb0916d
Otavio Santana <otaviopolianasantana@gmail.com>
2023-07-07 12:55:55 +0100

    feat: include a loop with app2
    Signed-off-by: Otavio Santana <otaviopolianasantana@gmail.com>

--- src/main/java/org/example/App.java
+++ src/main/java/org/example/App.java
@@ -5,6 +5,7 @@ import jakarta.enterprise.inject.se.SeContainer;
 import jakarta.enterprise.inject.se.SeContainerInitializer;
 import jakarta.nosql.Template;
 
+import java.util.List;
 import java.util.Optional;
 
 public class App
@@ -21,6 +22,12 @@ public class App
             Optional<Cat> optional = template.find(Cat.class, cat.id());
             System.out.println("The result: " + optional);
 
+            for (int index = 0; index < 100; index++) {
+                template.insert(Cat.create(faker));
+            }
+            List<Cat> result = template.select(Cat.class).where("breed").eq(cat.breed()).result();
+            System.out.println("The query by breed: " + result);
+            template.delete(Cat.class, cat.id());
         }
     }
 }
